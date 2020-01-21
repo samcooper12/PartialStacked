39a65f4749c09070b1c96e8a92e7623c598681fc
Sam Cooper <samcooperpdx@gmail.com>
2020-01-20 16:54:22 -0800

    more dfata

--- map.js
+++ map.js
@@ -1,5 +1,25 @@
 
 var counties = 'counties.json'
+
+var health = 'HEALTH.csv'
+
+var scores = []
+
+d3.csv(health, function(data){
+	console.log(data)
+
+	healthy = data
+
+	for (i = 0; i < healthy.length; i++){
+		scores.push(healthy[i]['PCT_OBESE_ADULTS13'])
+	}
+		console.log(scores)
+		// var 
+
+		return healthy, scores
+})
+
+console.log(scores)
 // Grabbing our GeoJSON data..
 d3.json(counties, function(data) {
 // var counties = '/data/counties.json'
@@ -9,14 +29,53 @@ var myMap = L.map("map", {
     zoom: 5
     // layers: [streetmap, counties]
   });
+
+
+
 L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
   attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
   maxZoom: 18,
   id: "mapbox.streets",
   accessToken: API_KEY
 }).addTo(myMap);
+
+// function onEachFeature(feature, layer) {
+//         layer.on({
+//             mouseover: highlightFeature
+//         });
+//     }
+
+  var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.streets",
+    accessToken: API_KEY
+  }).addTo(myMap)
+
+  var darkmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.dark",
+    accessToken: API_KEY
+  }).addTo(myMap)
+
+  // Define a baseMaps object to hold our base layers
+  var baseMaps = {
+    "Street Map": streetmap,
+    "Dark Map": darkmap
+  }
+
+  // Create overlay object to hold our overlay layer
+  var overlayMaps = {
+    Counties: counties
+  };
+// var controls = L.control.layers(baseMaps, overlayMaps, {
+//     collapsed: false
+//   }).addTo(myMap);
+
   // Creating a geoJSON layer with the retrieved data
   L.geoJson(data, {
+  	// onEachFeature: onEachFeature,
     // Style each feature (in this case a neighborhood)
     style: function(feature) {
       return {
@@ -27,128 +86,16 @@ L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
         fillOpacity: 0.5,
         weight: 1.5
       };
+      // onEachFeature: onEachFeature
     },
     // Called on each feature
   }).addTo(myMap);
+
+  var controls = L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(myMap);
 });
-// console.log(data)
 
-// //   }).addTo(myMap);
-// $( window ).on( "load", function(data) {
-// // });
-// // var e = data['features'][0]
-// // var e = data['type']
-// // console.log('e')
-// // console.log(e)
-
-// // console.log(data.length)
-// var myMap = L.map("map", {
-//   // Portland OR
-//     center: [45.51179, -122.67563],
-//   // US
-//     // center: [39.977825662466266, -98.934902125],
-//     // center: [45.51179, -100.67563],
-//     zoom: 5,
-//     maxZoom: 7
-//     // layers: [streetmap, counties]
-//   });
-
-//   var counties = L.geoJson(data, {
-
-//     // Style each feature (in this case a neighborhood)
-//     style: function(feature) {
-//       return {
-//         color: "white",
-//         fillColor: '#74B6E6',
-//         // fillOpacity: 0.5,
-//         weight: 1.5
-//       };
-//     },
-    
-//   }).addTo(myMap);
-
-
-//   var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-//     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-//     maxZoom: 18,
-//     id: "mapbox.streets",
-//     accessToken: API_KEY
-//   }).addTo(myMap)
-
-//   var darkmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-//     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-//     maxZoom: 18,
-//     id: "mapbox.dark",
-//     accessToken: API_KEY
-//   }).addTo(myMap)
-
-//   // Define a baseMaps object to hold our base layers
-//   var baseMaps = {
-//     "Street Map": streetmap,
-//     "Dark Map": darkmap
-//   }
-
-//   // Create overlay object to hold our overlay layer
-//   var overlayMaps = {
-//     Counties: counties
-//   };
-
-//   // Create our map, giving it the streetmap and earthquakes layers to display on load
-
-//   var controls = L.control.layers(baseMaps, overlayMaps, {
-//     collapsed: false
-//   }).addTo(myMap);
-
-// // var e = data['features'][0]
-// // console.log('e')
-// // console.log(e)
-// })
-
-
-// /*
-
-// var counties = 'data/counties.json'
-
-// var myMap = L.map("map", {
-//   // Portland OR
-//     center: [45.51179, -122.67563],
-//   // US
-//     // center: [39.977825662466266, -98.934902125],
-//     // center: [45.51179, -100.67563],
-//     zoom: 5,
-//     maxZoom: 7
-
-//     // layers: [streetmap, counties]
-//   });
-
-
-// d3.json(counties, function(data) {
-
-
-// L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-//   attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
-//   maxZoom: 18,
-//   // Grey background
-//   // id: "mapbox.grey",
-//   id: "mapbox.streets",
-//   accessToken: API_KEY
-// }).addTo(myMap);
-
-
-//   // Creating a geoJSON layer with the retrieved data
-//   L.geoJson(data, {
-
-//     // Style each feature (in this case a neighborhood)
-//     style: function(feature) {
-//       return {
-//         color: "white",
-//         fillColor: '#74B6E6',
-//         fillOpacity: 0.5,
-//         weight: 1.5
-//       };
-//     },
-    
-//   }).addTo(myMap);
-// })
-// });
-// */
+var controls = L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(myMap);
